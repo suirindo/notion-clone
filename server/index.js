83e32f550dcb9eb4150cb9d1a0db43ad3dd2c347
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-24 22:50:14 +0900

    0924

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// clientがルートディレクトリに対し、getメソッドを送ってきた際のリターン
+// req res を受け取る
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+app.listen(PORT, () => {
+  console.log('ローカルサーバー起動中...');
+});
