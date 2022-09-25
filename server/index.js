471ba8bbfbb13a0f4c52f8233765c0c4f9f37509
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-25 20:17:33 +0900

    0925

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const CryptoJS = require('crypto-js');
+const User = require('./src/v1/models/user');
 const app = express();
 const PORT = 5000;
+require('dotenv').config();
 
-// clientがルートディレクトリに対し、getメソッドを送ってきた際のリターン
-// req res を受け取る
-app.get('/', (req, res) => {
-  res.send('Hello Express');
+// DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log('DBと接続中・・・');
+} catch (error) {
+  console.log(error);
+}
+
+// ユーザー新規登録API
+app.post('/register', async (req, res) => {
+  // クライアントから入力されたパスワードの受け取り
+  const password = req.body.password;
+
+  try {
+    // パスワードの暗号化 第２引数は秘密鍵
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    // ユーザーの新規作成
+    const user = await User.create(req.body);
+  } catch {}
 });
+
+// ユーザーログイン用API
 
 app.listen(PORT, () => {
   console.log('ローカルサーバー起動中...');
