c875c17bd50af73f48c804b93f45381b52e45964
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-29 00:39:42 +0900

    0929

--- server/src/v1/routes/auth.js
+++ server/src/v1/routes/auth.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const User = require('../models/user');
 const validation = require('../handlers/validation');
 const userController = require('../controllers/user');
+const tokenHandler = require('../handlers/tokenHandler');
 
 // ユーザー新規登録API
 router.post(
@@ -46,7 +47,7 @@ router.post(
 
 // JWT認証API
 // JWT認証の手前にミドルウェアを設定して、これが正常に通ったら200が通るようにする
-router.post('/verfiy-token', (req, res) => {
+router.post('/verfiy-token', tokenHandler.verifyToken, (req, res) => {
   return res.status(200).json({ user: req.user });
 });
 
