f9f8775915c3dd476895bcecbb551c753c9b70a6
asagiman <lagavulin.jb.dc@gmail.com>
2022-10-02 18:33:00 +0900

    1002

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,11 +1,30 @@
+import { Box } from '@mui/material';
+import { Container } from '@mui/system';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import notionLogo from '../../assets/images/notion-logo.png';
 
 const AuthLayout = () => {
   return (
     <div>
-      AuthLayout
-      <Outlet />
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 6,
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <img
+            src={notionLogo}
+            alt=""
+            style={{ width: 100, height: 100, marginBottom: 3 }}
+          />
+          Notionクローン開発
+        </Box>
+        <Outlet />
+      </Container>
     </div>
   );
 };
