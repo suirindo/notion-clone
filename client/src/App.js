858d59afcd617b0101642dbf14d46531436a0637
asagiman <lagavulin.jb.dc@gmail.com>
2022-10-02 17:42:24 +0900

    1002

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AuthLayout from './components/layout/AuthLayout';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { blue } from '@mui/material/colors';
 
 function App() {
-  return <div className="App"></div>;
+  const theme = createTheme({
+    palette: { primary: blue },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
