1a103dee0ab2d3867246f465347fde11cd6f6422
asagiman <lagavulin.jb.dc@gmail.com>
2022-10-04 17:42:31 +0900

    1004

---
+++ client/src/api/axiosClient.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000/api/v1/';
+const getToken = () => localStorage.getItem('token');
+
+const axiosClient = axios.create({
+  baseURL: BASE_URL,
+});
+
+// APIを叩く前に前処理を行う
+axiosClient.interceptors.request.use(async (config) => {
+  return {
+    ...config,
+    header: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer${getToken()}`, // リクエストヘッダにJWTをつけてサーバーに渡す
+    },
+  };
+});
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (err) => {
+    throw err.response;
+  }
+);
+
+export default axiosClient;
--- server/index.js
+++ server/index.js
@@ -4,9 +4,16 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
 require('dotenv').config();
+const cors = require('cors');
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 // jsonオブジェクトを認識できるようにするための準備
 app.use(express.json());
-app.use('/api/v1', require('./src/v1/routes/auth'));
+app.use('/api/v1', require('./src/v1/routes'));
 
 // DB接続
 try {
